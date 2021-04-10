293b4d530bc8ebef49efe2762392d8be36cbd1d6
Luis Filippe Ullmann <filippemkt@gmail.com>
2021-04-09 21:24:15 -0300

    Completed and debugged

--- java.js
+++ java.js
@@ -31,64 +31,10 @@ var cartaGui = {
   },
 };
 
-var cartaLol = {
-  nome: "Caitlyn",
-  imagem:
-    "http://1.bp.blogspot.com/-K7CbqWc1-p0/VLc98v85s0I/AAAAAAAABqk/-ZB684VVHbg/s1600/Caitlyn_OriginalSkin.jpg",
-  atributos: {
-    ataque: 95,
-    defesa: 40,
-    magia: 10,
-  },
-};
-
-var cartaNaruto = {
-  nome: "Naruto",
-  imagem:
-    "https://conteudo.imguol.com.br/c/entretenimento/16/2017/06/27/naruto-1498593686428_v2_450x337.png",
-  atributos: {
-    ataque: 80,
-    defesa: 60,
-    magia: 100,
-  },
-};
-
-var cartaHarry = {
-  nome: "Harry Potter",
-  imagem:
-    "https://sm.ign.com/ign_br/screenshot/default/89ff10dd-aa41-4d17-ae8f-835281ebd3fd_49hp.jpg",
-  atributos: {
-    ataque: 70,
-    defesa: 50,
-    magia: 95,
-  },
-};
-
-var cartaBatman = {
-  nome: "Batman",
-  imagem:
-    "https://assets.b9.com.br/wp-content/uploads/2020/09/Batman-issue86-heder-1280x677.jpg",
-  atributos: {
-    ataque: 95,
-    defesa: 70,
-    magia: 0,
-  },
-};
-
-var cartaMarvel = {
-  nome: "Capitã Marvel",
-  imagem:
-    "https://cinepop.com.br/wp-content/uploads/2018/09/capitamarvel21.jpg",
-  atributos: {
-    ataque: 90,
-    defesa: 80,
-    magia: 0,
-  },
-};
-
 var cartaMaquina;
 var cartaJogador;
 var cartas = [cartaPaulo, cartaRafa, cartaGui];
+// 0          1           2
 
 function sortearCarta() {
   var numeroCartaMaquina = parseInt(Math.random() * 3);
@@ -103,29 +49,32 @@ function sortearCarta() {
 
   document.getElementById("btnSortear").disabled = true;
   document.getElementById("btnJogar").disabled = false;
-  exibirOpcoes();
+
+  exibeCartaJogador();
 }
 
 function exibeCartaJogador() {
   var divCartaJogador = document.getElementById("carta-jogador");
-
-  divCartaJogador.style.backgroundImage = "url(${cartaJogador.imagem})";
-  var nome = '<p class= "carta-subtitle">${cartaJogador.nome}</p>';
-
-  divCartaJogador.innerHTML = nome;
-}
-
-function exibirOpcoes() {
-  var opcoes = document.getElementById("opcoes");
+  var moldura =
+    '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" style=" width: inherit; height: inherit; position: absolute;">';
+  divCartaJogador.style.backgroundImage = `url(${cartaJogador.imagem})`;
+  var nome = `<p class="carta-subtitle">${cartaJogador.nome}</p>`;
   var opcoesTexto = "";
+
   for (var atributo in cartaJogador.atributos) {
     opcoesTexto +=
       "<input type='radio' name='atributo' value='" +
       atributo +
       "'>" +
-      atributo;
+      atributo +
+      " " +
+      cartaJogador.atributos[atributo] +
+      "<br>";
   }
-  opcoes.innerHTML = opcoesTexto;
+
+  var html = "<div id='opcoes' class='carta-status'>";
+
+  divCartaJogador.innerHTML = moldura + nome + html + opcoesTexto + "</div>";
 }
 
 function obtemAtributoSelecionado() {
@@ -138,20 +87,48 @@ function obtemAtributoSelecionado() {
 }
 
 function jogar() {
+  var divResultado = document.getElementById("resultado");
   var atributoSelecionado = obtemAtributoSelecionado();
 
   if (
     cartaJogador.atributos[atributoSelecionado] >
     cartaMaquina.atributos[atributoSelecionado]
   ) {
-    alert("Venceu a carta máquina");
+    htmlResultado = '<p class="resultado-final">Venceu</p>';
   } else if (
     cartaJogador.atributos[atributoSelecionado] <
     cartaMaquina.atributos[atributoSelecionado]
   ) {
-    alert("Perdeu. Carta da máquina é maior");
+    htmlResultado = '<p class="resultado-final">Perdeu</p>';
   } else {
-    alert("Empatou!");
+    htmlResultado = '<p class="resultado-final">Empatou</p>';
   }
-  console.log(cartaMaquina);
+
+  divResultado.innerHTML = htmlResultado;
+  exibeCartaMaquina();
+}
+
+function exibeCartaMaquina() {
+  var divCartaMaquina = document.getElementById("carta-maquina");
+  var moldura =
+    '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" style=" width: inherit; height: inherit; position: absolute;">';
+  divCartaMaquina.style.backgroundImage = `url(${cartaMaquina.imagem})`;
+  var nome = `<p class="carta-subtitle">${cartaMaquina.nome}</p>`;
+  var opcoesTexto = "";
+
+  for (var atributo in cartaMaquina.atributos) {
+    console.log(atributo);
+    opcoesTexto +=
+      "<p type='text' name='atributo' value='" +
+      atributo +
+      "'>" +
+      atributo +
+      " " +
+      cartaMaquina.atributos[atributo] +
+      "<br>";
+  }
+
+  var html = "<div id='opcoes' class='carta-status --spacing'>";
+
+  divCartaMaquina.innerHTML = moldura + nome + html + opcoesTexto + "</div>";
 }
